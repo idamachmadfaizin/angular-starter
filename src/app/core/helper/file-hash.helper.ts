d90d03636049de25b3028c556d7f97bcba4befbd
Idam <i.faizin@cebes.co.id>
2020-03-28 10:38:15 +0700

    hashing

--- src/app/core/helper/file-hash.helper.ts
+++ src/app/core/helper/file-hash.helper.ts
@@ -1,10 +1,55 @@
-export class FileHash {
-    public static base64 = (file: Blob) => new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve((reader.result.toString()).split('base64,')[1]);
-        reader.onerror = error => reject(error);
-    })
+import { sha256, sha224 } from 'js-sha256';
 
-    public static sha256() { }
+export class FileHash {
+    // public static base64 = (file: Blob) => new Promise((resolve, reject) => {
+    //     const reader = new FileReader();
+    //     reader.readAsDataURL(file);
+    //     reader.onload = () => resolve((reader.result.toString()).split('base64,')[1]);
+    //     reader.onerror = error => reject(error);
+    // })
+
+    /**
+     * Convert file to base64
+     * @param file file in blob type
+     */
+    public static async base64(file: Blob) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve((reader.result.toString()).split('base64,')[1]);
+            reader.onerror = error => reject(error);
+        }).catch(err => Error(err));
+    }
+
+    /**
+     * Convert file to sha256 checksum
+     * @param file file in blob type
+     */
+    public static async sha256(file: Blob) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            // reader.readAsDataURL(file);
+            reader.onload = (f) => {
+                resolve(sha256(reader.result));
+            };
+            reader.onerror = error => reject(error);
+            reader.readAsArrayBuffer(file);
+        }).catch(err => Error(err));
+    }
+
+    /**
+     * Convert file to sha224 checksum
+     * @param file file in blob type
+     */
+    public static async sha224(file: Blob) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            // reader.readAsDataURL(file);
+            reader.onload = (f) => {
+                resolve(sha224(reader.result));
+            };
+            reader.onerror = error => reject(error);
+            reader.readAsArrayBuffer(file);
+        }).catch(err => Error(err));
+    }
 }
--- src/app/core/helper/hash.helper.ts
+++ src/app/core/helper/hash.helper.ts
@@ -1,5 +1,25 @@
-export class Hash {
-    public static base64() { }
+import { sha256, sha224 } from 'js-sha256';
+import { sha3_256, sha3_512 } from 'js-sha3';
 
-    public static sha256() { }
+export class Hash {
+    /**
+     * Convert string to sha256
+     * @param text string
+     */
+    public static sha256(text: string) { return sha256(text); }
+    /**
+     * Convert string to sha224
+     * @param text string
+     */
+    public static sha224(text: string) { return sha224(text); }
+    /**
+     * Convert string to sha3-256
+     * @param text string
+     */
+    public static sha3_256(text: string) { return sha3_256(text); }
+    /**
+     * Convert string to sha3-512
+     * @param text string
+     */
+    public static sha3_512(text: string) { return sha3_512(text); }
 }
